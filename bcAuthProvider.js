43f0777ea71c2723286993ab1a4f13db6cf431ef
Mario Couture <MarioC@bitheads.com>
2019-04-24 10:24:34 -0400

    Fixed session expired not being properly detected.

--- bcAuthProvider.js
+++ bcAuthProvider.js
@@ -1,10 +1,4 @@
-import {
-    AUTH_LOGIN,
-    AUTH_LOGOUT,
-    AUTH_CHECK,
-    AUTH_ERROR,
-    AUTH_GET_PERMISSIONS
-} from 'react-admin';
+import { AUTH_CHECK, AUTH_ERROR, AUTH_GET_PERMISSIONS, AUTH_LOGIN, AUTH_LOGOUT } from 'react-admin';
 
 export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
 
@@ -12,8 +6,8 @@ export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
     var _hasLoggedin = false;
     var _currentProfile = {};
     var _currentPermission = null;
-    var _roleAttribute = roleAttribute; 
-    
+    var _roleAttribute = roleAttribute;
+
     function validateLogin(result, resolve, reject) {
         if (verbose) console.log("---> Loged-in to braincloud ", result);
         if (result.status === 200) {
@@ -24,13 +18,13 @@ export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
                 var status = result.status;
                 if (status === 200) {
                     _currentPermission = result.data.attributes[_roleAttribute];
-                    localStorage.setItem(_bc.wrapperName+".permission",_currentPermission);
+                    localStorage.setItem(_bc.wrapperName + ".permission", _currentPermission);
                 }
-                if (verbose) console.log("---> User permission from %s is %s",_roleAttribute, _currentPermission);                
+                if (verbose) console.log("---> User permission from %s is %s", _roleAttribute, _currentPermission);
                 resolve()
             });
         } else {
-            reject("Error" + result.status);
+            reject(result);
         }
     }
 
@@ -45,13 +39,18 @@ export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
             if (verbose) console.log("---> Login", params);
             return new Promise(function (resolve, reject) {
                 _bc.authenticateEmailPassword(username, password, false, result => {
-                    validateLogin(result,resolve,reject);
+                    validateLogin(result, resolve, reject);
                 });
             });
         }
         if (type === AUTH_LOGOUT) {
             _hasLoggedin = false;
-            return Promise.resolve();
+            return new Promise(function (resolve, reject) {
+                _bc.playerState.logout(result => {
+                    if (verbose) console.log("---> _bc.playerState.logout ", result);
+                    return resolve();
+                });
+            })
         }
         if (type === AUTH_ERROR) {
             const status = params.status;
@@ -64,8 +63,7 @@ export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
             return Promise.resolve();
         }
         if (type === AUTH_CHECK) {
-            const hasSessionId = _bc.brainCloudClient.isAuthenticated() || window.localStorage.getItem(_bc.wrapperName +".sessionId") !== null;
-            if (verbose) console.log("---> _bc.brainCloudClient.isAuthenticated() = %s",hasSessionId);
+            const hasSessionId = _bc.brainCloudClient.isAuthenticated() ||_bc.getSessionId() !== null;
             if (_hasLoggedin && hasSessionId) {
                 if (verbose) console.log("---> Already logged-in and authenticated");
                 return Promise.resolve()
@@ -74,7 +72,7 @@ export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
                 return new Promise(function (resolve, reject) {
                     if (verbose) console.log("---> Attempting to restoring Session...");
                     _bc.restoreSession(result => {
-                        validateLogin(result,resolve,reject);
+                        validateLogin(result, resolve, reject);
                     });
                 })
             } else {
@@ -83,9 +81,15 @@ export default (bc, roleAttribute = "react-admin-role", verbose = false) => {
             }
         }
         if (type === AUTH_GET_PERMISSIONS) {
-            _currentPermission = localStorage.getItem(_bc.wrapperName+".permission");
-            if (verbose) console.log("---> Getting permission %s",_currentPermission);
-            return Promise.resolve(_currentPermission);
+            const hasSessionId = _bc.brainCloudClient.isAuthenticated() ||_bc.getSessionId() !== null;
+            if (hasSessionId) {
+                _currentPermission = localStorage.getItem(_bc.wrapperName + ".permission");
+                if (verbose) console.log("---> Getting permissions ", _currentPermission);
+                return Promise.resolve(_currentPermission);
+            } else {
+                if (verbose) console.log("---> No permissions available");
+                return Promise.reject({ status: 500, status_message: "no permission available" });
+            }
         }
 
         if (verbose) console.warn("---> Unknown type %s", type);
